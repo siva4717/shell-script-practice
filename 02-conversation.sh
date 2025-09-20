4385ce5fb4a3bd833a6db089102c65c5ad32c80b
siva4717 <sivagopi@4717>
2025-09-20 12:42:00 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#?/bin/bash
+
+echo "Ramesh::Hi Suresh how are you?"
+echo "Suresh::Hi Ramesh iam fine, what about you?"
+echo "Ramesh::iam also fine what are you learning?"
+echo "Suresh::iam learning devops in join devops Ramesh."
