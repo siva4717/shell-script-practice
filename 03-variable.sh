11c95bf6f43dab883d836c47b5627923f520d4a4
siva4717 <sivagopi@4717>
2025-09-20 12:46:51 +0530

    create two persons variables

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#?/bin/bash
+PERSON1=John
+PERSON2=Raheem
+echo "$PERSON1::Hi $PERSON2 how are you?"
+echo "$PERSON2::Hi $PERSON1 iam fine, what about you?"
+echo "$PERSON1::iam also fine what are you learning?"
+echo "$PERSON2::iam learning devops in join devops $PERSON1."
