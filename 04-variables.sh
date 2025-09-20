4d6a8fb914e927fb5e21856f3d0034fa8b594062
siva4717 <sivagopi@4717>
2025-09-20 14:12:45 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,6 +1,7 @@
 #?/bin/bash
 PERSON1=$1
 PERSON2=$2
+
 echo "$PERSON1::Hi $PERSON2 how are you?"
 echo "$PERSON2::Hi $PERSON1 iam fine, what about you?"
 echo "$PERSON1::iam also fine what are you learning?"
