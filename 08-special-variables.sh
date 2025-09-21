99163060a9cd7bbda5ff9c13e0ced48e02e83d94
siva4717 <sivagopi@4717>
2025-09-21 14:07:28 +0530

    date variable

---
+++ 08-special-variables.sh
@@ -0,0 +1,10 @@
+#?/bin/bash
+echo "All variables passed to the script:$@"
+echo "All variables passed to the script:$*"
+echo "Script name:$0"
+echo "current directory:$pwd"
+echo "who is running this:$user"
+echo "Home directory of user:$home"
+echo "PID of this script:$$"
+sleep 50 &
+echo "PID of the last command of background is:$!"
