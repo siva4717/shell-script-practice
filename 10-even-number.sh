ea10a5b8a5fe43161050ea695daed0ba944c3d24
siva4717 <sivagopi@4717>
2025-09-23 14:17:54 +0530

    add if-else statement

--- 10-even-number.sh
+++ 10-even-number.sh
@@ -1,7 +1,7 @@
 #?/bin/bash
 echo "please enter the number"
-read NUMBEER
-if [ $(( $NUMBER % 2 )) -eq 0 ]; then 
+read NUMBER
+if [ $(($NUMBER%2)) -eq 0 ]; then 
     echo "The given number $NUMBER is EVEN"
 else
     echo "The given number $NUMBER is ODD"
