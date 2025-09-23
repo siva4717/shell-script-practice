d663dd6769d212d1377bf0888974ee720d763384
siva4717 <sivagopi@4717>
2025-09-23 14:02:34 +0530

    add if-else statement

--- 09-if-else.sh
+++ 09-if-else.sh
@@ -1,7 +1,7 @@
 #?/bin/bash
 NUMBER=$1
-if [$NUMBER -lt 10]; then
-    echo "The given number $1 is lessthan 10"
+if [ $NUMBER -lt 10]; then
+    echo "The given number $NUMBER is lessthan 10"
 else
-    echo "The given number $1 is greater than or equal to 10"
+    echo "The given number $NUMBER is greater than or equal to 10"
 fi
