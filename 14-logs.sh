1f094f9e423f5e08945fd8eced63f38572d1a8bc
siva4717 <sivagopi@4717>
2025-09-23 15:11:32 +0530

    LOGS

--- 14-logs.sh
+++ 14-logs.sh
@@ -2,11 +2,12 @@
 USER_ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 FILE_LOG_DIRECTORY="/var/log/shell-script/"
 SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
 FILE_LOG=$FILE_LOG_DIRECTORY/$SCRIPT_NAME.log
-mkdir -p $FILE_LOG_DIRECTORY
+mkdir -p $FILE_LOG_DIRECTORY    
 
 if [ $USER_ID -ne 0 ]; then 
     echo -e " $R You can use root user $N" 
