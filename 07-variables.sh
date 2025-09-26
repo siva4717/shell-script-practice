2987c71cd932f4b9a88044da906b6de3b4fc6eed
siva4717 <sivagopi@4717>
2025-09-26 15:16:02 +0530

    shell-roboshop

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,7 +1,11 @@
 #?/bin/bash
 
-START_DATE=$(date +%s)
-sleep 30
-END_DATE=$(date +%s)
-TOTAL_TIME=$(($END_DATE-$START_DATE))
-echo "the process execution time is $TOTAL_TIME"
+DATE=$(date)
+
+echo "Today date:$DATE"
+
+START_TIME=$(date +"%s")
+sleep 5
+END_TIME=$(date +"%s")
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "script executed in $TOTAL_TIME seconds"
