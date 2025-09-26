ed77fb042f23a0c240af6fd9f764bf39ed49caf5
siva4717 <sivagopi@4717>
2025-09-26 14:28:22 +0530

    shell-roboshop

--- root_user.sh
+++ root_user.sh
@@ -8,6 +8,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+script_name=$(echo $0 | awk -d '.' -f1)
+FILE_LOG_DIRECTORY="/var/log/shell-script"
+FILE_LOG=$FILE_LOG_DIRECTORY/$script_name.log
+
 echo "script starting time is:$(date +"%F.%H.%M.%S")"
 echo -e "$G user: $user_id $N"
 
@@ -23,23 +27,23 @@ echo "root user:$user_id"
 
 validate(){
     if [ $1 -ne 0 ]; then 
-        echo -e "$package $R Erorr $N"
+        echo -e "$package $R Erorr $N" -tee &>> $FILE_LOG
     else
-        echo -e "Installing $package $G success $N"
+        echo -e "Installing $package $G success $N" -tee &>> $FILE_LOG
     fi
 }
 
 for package in $@
 do
-    dnf list $package 
+    dnf list $package &>> $FILE_LOG
     if [ $? != 0 ]; then
-        dnf install $package -y 
-        validate $? "$package"
+        dnf install $package -y &>> $FILE_LOG
+        validate $? "$package" -tee &>> $FILE_LOG
     else
         echo -e "$package is already installed $Y skip $N"
     fi
 done
-script_name=$0
+
 echo "$script_name"
 END_time=$(date +"%s")
 TOTAL_TIME=$(($END_time-$start_time))
