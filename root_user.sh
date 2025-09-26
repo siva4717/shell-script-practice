84a04afd8ec8e197c3b0313354fe4df0590f0469
siva4717 <sivagopi@4717>
2025-09-26 13:13:15 +0530

    root user

---
+++ root_user.sh
@@ -0,0 +1,14 @@
+#?/bin/bash
+
+#check the root user exist or not
+
+user_id=$(id)
+
+if [ $user_id -ne 0 ]; then
+    echo "please use the root user"
+    exit 1
+else
+    echo "you are connecting root user"
+fi
+
+echo "root user"
