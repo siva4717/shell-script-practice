12adc402fee2192dbbc760e6126b8d220068792a
siva4717 <sivagopi@4717>
2025-09-26 15:40:12 +0530

    shell-roboshop

--- 09-if-else.sh
+++
@@ -1,9 +0,0 @@
-#?/bin/bash
-NUMBER=$1
-if [ $NUMBER -lt 10 ]; then
-    echo "The given number $NUMBER is lessthan 10"
-elif [ $NUMBER -eq 10 ]; then
-    echo "The given number $NUMBER is equal to 10"
-else
-    echo "The given number $NUMBER is greater than 10"
-fi
---
+++ 10-if-else.sh
@@ -0,0 +1,14 @@
+#?/bin/bash
+NUMBER=$1
+if [ $NUMBER -lt 10 ]; then 
+    echo "The give number $NUMBER is lessthan to 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "The given number $NUMBER is equal to 10"
+else
+    echo "The given number $NUMBER is greaterthan 10"
+fi
+
+# -lt -->lessthan
+# -gt -->greaterthan
+# -eq -->equal
+# -ne -->not equal
