a5c28bf5f37d5041784ba03801595428ff1b7f21
siva4717 <sivagopi@4717>
2025-09-26 15:43:29 +0530

    shell-roboshop

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,8 +1,8 @@
 #?/bin/bash
 NUMBER=$1
-if [ $NUMBER -lt 10 ]; then 
+if [ $NUMBER < 10 ]; then 
     echo "The given number $NUMBER is lessthan to 10"
-elif [ $NUMBER -eq 10 ]; then
+elif [ $NUMBER == 10 ]; then
     echo "The given number $NUMBER is equal to 10"
 else
     echo "The given number $NUMBER is greaterthan 10"
