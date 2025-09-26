4e98d7f6a1813ccacf52681a01b451aa24023d16
siva4717 <sivagopi@4717>
2025-09-26 14:54:20 +0530

    shell-roboshop

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #?/bin/bash
 
-echo "Ramesh::Hi Suresh how are you?"
-echo "Suresh::Hi Ramesh iam fine, what about you?"
-echo "Ramesh::iam also fine what are you learning?"
-echo "Suresh::iam learning devops in join devops Ramesh."
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am learning Shell scripting Ramesh"
