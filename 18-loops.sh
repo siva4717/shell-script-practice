99161dd09276db298ec16cd36aa5069950f51c99
siva4717 <sivagopi@4717>
2025-09-26 17:05:33 +0530

    shell-roboshop

---
+++ 18-loops.sh
@@ -0,0 +1,39 @@
+#?/bin/bash
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+FILE_LOG_DIRECTORY="/var/log/shell-script/"
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
+FILE_LOG=$FILE_LOG_DIRECTORY/$SCRIPT_NAME.log
+mkdir -p $FILE_LOG_DIRECTORY 
+echo -e "$G The script Started at ::: $(date)$N"
+
+if [ $USER_ID -ne 0 ]; then 
+    echo -e " $R You can use root user $N" 
+    exit 1
+    
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R failure $N "
+    else
+        echo -e "Installing $2 ... $G success $N "
+    fi
+}
+#$@
+for package in $@
+do
+    #check package is installed or not 
+    dnf list installed $package &>>$FILE_LOG
+    #if exit status is 0 it is already installed -ne 0 then install the package
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$FILE_LOG
+        VALIDATE $? $package 
+    else
+        echo "$package is already installed"
+    fi
+
+done
