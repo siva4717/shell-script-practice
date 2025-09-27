3ca019ac7ffc1b370288f8e4fc1ccdff93ce16af
siva4717 <sivagopi@4717>
2025-09-27 13:35:31 +0530

    message

---
+++ 21-script.sh
@@ -0,0 +1,6 @@
+#?/bin/bash
+
+NAME="india"
+echo "my country name is $NAME "
+echo "the PID of first script:$$"
+sh 22-script.sh
---
+++ 22-script.sh
@@ -0,0 +1,4 @@
+#?/bin/bash
+
+echo "my country name is $NAME"
+echo "pid of script 2 is $$"
